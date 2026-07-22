cda7adfa0a2ae256435a517e1d0c16fa08a0ea51
NilsonAbrantes <lucasnilson624@gmail.com>
2026-07-22 14:51:46 -0300

    fix: remover configuracao duplicada do SQLite

--- financeiro_project/settings.py
+++ financeiro_project/settings.py
@@ -94,13 +94,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "financeiro_project.wsgi.application"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 LANGUAGE_CODE = "pt-br"
 TIME_ZONE = "America/Fortaleza"
 USE_I18N = True
